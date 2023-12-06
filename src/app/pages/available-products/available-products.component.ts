fbe7f006f56957bc077b112be2dfc6a263c3b625
Joselu <joseluissgz97@gmail.com>
2023-12-06 19:45:06 +0100

    available-products page

---
+++ src/app/pages/available-products/available-products.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-available-products',
+  templateUrl: './available-products.component.html',
+  styleUrls: ['./available-products.component.scss']
+})
+export class AvailableProductsComponent {
+
+}
