c9d0ddc03b7de9a55dc5b115f5e87af05faf2889
Joselu <joseluissgz97@gmail.com>
2023-12-06 23:18:03 +0100

    importing loader

--- src/app/pages/products.module.ts
+++ src/app/pages/products.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ProductCardComponent } from '../components/product-card/product-card.component';
+import { LoaderComponent } from '../shared/loader/loader.component';
 import { AllProductsComponent } from './all-products/all-products.component';
 import { AvailableProductsComponent } from './available-products/available-products.component';
 import { ProductsRoutingModule } from './prducts-routing.module';
@@ -12,6 +13,7 @@ import { ProductsComponent } from './products/products.component';
     AvailableProductsComponent,
     ProductCardComponent,
     ProductsComponent,
+    LoaderComponent,
   ],
   imports: [CommonModule, ProductsRoutingModule],
 })
